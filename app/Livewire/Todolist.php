35f79a3d9d9e19c193485fec309e18928957d151
scilentgemini <rbchitthi@gmail.com>
2025-05-30 10:41:59 +0545

    C completed of CRUD

--- app/Livewire/Todolist.php
+++ app/Livewire/Todolist.php
@@ -2,12 +2,34 @@
 
 namespace App\Livewire;
 
+use App\Models\Todo;
 use Livewire\Component;
+use Livewire\Attributes\Rule;
+use Livewire\WithPagination;
 
 class Todolist extends Component
 {
+    use WithPagination;
+
+    #[Rule('required|min:3|max:30')]
+    public $name;
+
+    public $search;
+
+    public function create(){
+        $validated = $this->validateOnly('name');
+
+        Todo::create($validated);
+
+        $this -> reset('name');
+
+        session()->flash('success', 'Created!');
+    }
+
     public function render()
     {
-        return view('livewire.todolist');
+        return view('livewire.todolist',[
+            'todos' => Todo::latest()->paginate()
+        ]);
     }
 }
